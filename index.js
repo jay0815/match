93fa030c9b5c45622d600576bf2ffc7e7bd066d3
jack <543612779@qq.com>
2020-05-15 11:50:21 +0800

    Executed 9 of 9 SUCCESS

--- index.js
+++ index.js
@@ -5,36 +5,26 @@ module.exports.match = function match(pattern, string) {
   let index = 0;
   // 当前探索的位置
   let site = 0;
-  for(;site < string.length;site++){
+  for (let c of string) {
     if(index < state.length ){
-      if(state[index] === string[site]){
+      if(state[index] === c){
         index ++;
-      }else if(state[index-1] === string[site]){
-        // 调整 游标起点
-        // index -= 1;
-      }else if(state[0] === string[site]){
+      }else if(state[index-1] === c && string.slice(site - index + 1, site) === state.slice(0, index-1).join('')){
+        // 判断是否需要对游标结果进行记录
+        // 当前节点与前一个节点的value相同时
+        // 将尝试比较已对比对象的值的整体范围向后移动1位
+        // 此时如果 新范围内的值与 目前已比较过的pattern内的值相同
+        // 通过暂停计数的方式,保留之前的结果
+      }else if(state[0] === c){
         // 调整 游标起点
         index = 1;
       }else {
         index = 0;
       }
+      site++;
     }else {
       break;
     }
   }
-  // for (let c of string) {
-  //   if(index < state.length ){
-  //     if(state[index] === c){
-  //       index ++;
-  //     }else if(state[0] === c){
-  //       // 调整 游标起点
-  //       index = 1;
-  //     }else {
-  //       index = 0;
-  //     }
-  //   }else {
-  //     break;
-  //   }
-  // }
   return index === state.length;
 }
